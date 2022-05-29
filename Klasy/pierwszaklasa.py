8c84420ae934da4ffe08b27e56923f7683e67941
IlonaRudnicka <IlonaRudnicka@users.noreply.github.com>
2022-05-29 13:01:13 +0200

    Implementation __init__ function

--- Klasy/pierwszaklasa.py
+++ Klasy/pierwszaklasa.py
@@ -9,15 +9,33 @@
 #    pass
 # bo nie wiemy, jak napisac klase odejmij, ale chcemy, zeby dzialalo
 
+
+# self reprezentuje instancje klasy, dzieki uzyciu self
+#my mozemy miec dostep do wlasciwosci i metod klasy w Pythonie
+
+#tworzenie obiektu realizujemy przez tzw. konstruktor - jest to spejalna metoda,
+#ktora jest wykonywana, kiedy tworzymy nasz obiekt.
+#W Pythonie taka funkcje pelni metoda __init__
 class Paletka:
-    pass #instrukcja nullowa (od null) - cos jak komentarz, ale interpreter tego nie ignoruje
+    def __init__(self, kolor):
+        self.kolor_obiektu = kolor
+        print(f"Utworzylismy obiekt o kolorze {self.kolor_obiektu} - ID: {id(self)}")
+    #pass #instrukcja nullowa (od null) - cos jak komentarz, ale interpreter tego nie ignoruje
 
 #tworzymy obiekt na podstawie klasy, podajemy nazwe obiektu (paletka_a) i wywolujemy konstruktor klasy (Paletka())
 
 def testklasy():
-    paletka_a = Paletka()
+    paletka_a = Paletka("czerwony")
+    paletka_b = Paletka("niebieski")
+    print("*******************************")
     print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne wlasciwosci i metody:")
     print(dir(paletka_a))
+    print("*******************************")
+    print(f"Obiekt typu {type(paletka_b)} zawiera od razu pewne wlasciwosci i metody:")
+    print(dir(paletka_b))
+    print("*******************************")
+    print(f"Kolor dla paletka_a: {paletka_a.kolor_obiektu}")
+    print(f"Kolor dla paletka_b: {paletka_b.kolor_obiektu}")
 
 #f-string
 #val = 'Python course'
